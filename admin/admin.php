189a527857a8a609b5a70392e124faba9b04412a
Cj Aguilar <rojayfire@gmail.com>
2023-11-06 16:21:11 +0800

    updates

--- admin/admin.php
+++ admin/admin.php
@@ -10,30 +10,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Summerhouse Cafe</title>
-    <link rel="stylesheet" href="../css/myStyles.css">
+    <link rel="stylesheet" href="../css/summerStyle.css">
     <link rel="stylesheet" href="../api/datatable.css">
     <script src="../api/datatable.js"></script>
     
 </head>
 <body>
-        <header class="p-3 header-style"">
+        <header class="p-2 header-style">
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start head-nav">
-                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-                    </a>
                     <a href="admin.php" class="nav-link px-2 text-white"><img src="../images/logo.png" alt="logo" style="height: 90px;width: 90px;border-radius: 50%;object-fit: contain"></a>
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li class="nav-bar"><a href="" class="nav-link px-2 text-white" id="active">Home</a></li>
-                    <li class="nav-bar"><a href="orders.php" class="nav-link px-2 text-white">Orders</a></li>
-                    <li class="nav-bar"><a href="reports.php" class="nav-link px-2 text-white">Reports</a></li>
-                    <li class="nav-bar"><a href="products.php" class="nav-link px-2 text-white">Products</a></li>
-                    <li class="nav-bar"><a href="categories.php" class="nav-link px-2 text-white">Categories</a></li>
-                    <li class="nav-bar"><a href="accounts.php" class="nav-link px-2 text-white">Accounts</a></li>
+                    <li class="nav-bar mt-2"><a href="" class="nav-link px-2 text-white" id="active">Home</a></li>
+                    <li class="nav-bar mt-2"><a href="orders.php" class="nav-link px-2 text-white">Orders</a></li>
+                    <li class="nav-bar mt-2"><a href="reports.php" class="nav-link px-2 text-white">Reports</a></li>
+                    <li class="nav-bar mt-2"><a href="products.php" class="nav-link px-2 text-white">Products</a></li>
+                    <li class="nav-bar mt-2"><a href="categories.php" class="nav-link px-2 text-white">Categories</a></li>
+                    <li class="nav-bar mt-2"><a href="accounts.php" class="nav-link px-2 text-white">Accounts</a></li>
                     </ul>
                     <div class="text-end">
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <span class="text-white">Welcome, <?php echo $_SESSION['displayName']."! "."(".$_SESSION['roleType']." - ".$_SESSION['roleDes'].")" ?></span>
+                    <span class="text-white welcome">Welcome, <?php echo $_SESSION['displayName']."! "."(".$_SESSION['roleType']." - ".$_SESSION['roleDes'].")" ?></span>
                     <a class="checkOut" href="../logout.php">Logout</a>
                     </div>
                     </form>
@@ -41,9 +38,8 @@
             </div>
         </header>
         <div class="container" id="order">
-        <div class="row">
         <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" class="mt-5">
-                <select class="dropdown" id="categoryId" name="categoryId" onchange="this.form.submit();">
+                <select class="dropdown mb-3" id="categoryId" name="categoryId" onchange="this.form.submit();">
                 <option value="" class="btn btn-secondary dropdown-toggle" style="background-color: rgb(255, 128, 0);">Choose Categories...</option>
                 <?php
                     foreach($categories as $category){
@@ -55,23 +51,24 @@
                 ?>
                 </select>
         </form>
-            <div class="col-sm sidebar-left">
-                <?php
+        <div class="rows">
+            <div class="side-left">
+            <?php
                     foreach($data as $row){
                 ?>
             <form action="admin.php?id=<?php echo  $row['product_id']; ?>" method="POST">
                     <div class="food-container">
-                        <div><img style="height: 150px;width: 250px; object-fit: cover;" src="<?php echo $row['image']; ?>"></div>
-                        <div style="color: #fff;"><?php echo $row['product_name']; ?></div>
-                        <div>₱<?php echo $row['price']; ?></div>
-                        <div><input type="number" name="txtQuan" min="0" value="1" required></div>
-                        <div><button name="addList">Add to list</button></div>
+                        <div class="m-2"><img style="height: 150px;width: 250px; object-fit: cover;" src="<?php echo $row['image']; ?>"></div>
+                        <div class="m-2"><?php echo $row['product_name']; ?></div>
+                        <div class="m-2">₱<?php echo $row['price']; ?></div>
+                        <div class="m-2"><input type="number" name="txtQuan" class="text-center" min="0" value="1" required></div>
+                        <div class="m-2 p-2"><button name="addList">Add to list</button></div>
                     </div>
             </form>
                 <?php
                     }
                 ?>
-        </div>
+            </div>
         <div class="col-sm sidebar-right">
             <header class="wrap mt-2">
                 <h1>Order List</h1>
@@ -93,7 +90,7 @@
                     <div class="food-wrap">
                         <div>
                             <p><?php echo $foodRow['product_name'];  ?>
-                           ₱<?php echo $foodRow['price']; ?></p>
+                            ₱<?php echo $foodRow['price']; ?></p>
                         </div>
                         <form action="admin.php?id=<?php echo $key; ?>" method="post" style="text-align: right;">
                             <button type="submit" name="btn-minus">-</button>
@@ -104,14 +101,14 @@
                     <?php
                         }}
                     ?>
-                    <div>Total: ₱<?php echo number_format($grandTotal); ?>
+                    <div class="m-3">Total: ₱<?php echo number_format($grandTotal); ?>
                     <br>
                     <p><?php echo $_SESSION['msg']; ?></p>
                     <br>
                     </div>
-                    <form method="post"><div><button name="checkOut" type="button" class="checkOut" data-bs-toggle="modal" data-bs-target="#myModal">Check Out</button></div></form>
+                    <form method="post"><div><button name="checkOut" type="button" id="checkOut" class="checkOut" data-bs-toggle="modal" data-bs-target="#myModal">Check Out</button></div></form>
                 </header>
-        </div>
+            </div>
 
                 <!-- Modal -->
                 <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
--- admin/products.php
+++ admin/products.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Summerhouse Cafe</title>
-    <link rel="stylesheet" href="../css/myStyle.css">
+    <link rel="stylesheet" href="../css/summerStyle.css">
     <link rel="stylesheet" href="../api/datatable.css">
     <script src="../api/datatable.js"></script>
     <style>
@@ -24,20 +24,17 @@
     </style>
 </head>
 <body>
-    <header class="p-3 header-style"">
+    <header class="p-2 header-style"">
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-                    </a>
                     <a href="products.php" class="nav-link px-2 text-white"><img src="../images/logo.png" alt="logo" style="height: 90px;width: 90px;border-radius: 50%;object-fit: contain"></a>
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li class="nav-bar"><a href="admin.php" class="nav-link px-2 text-white">Home</a></li>
-                    <li class="nav-bar"><a href="orders.php" class="nav-link px-2 text-white">Orders</a></li>
-                    <li class="nav-bar"><a href="reports.php" class="nav-link px-2 text-white">Reports</a></li>
-                    <li class="nav-bar"><a href="" class="nav-link px-2 text-white" id="active">Products</a></li>
-                    <li class="nav-bar"><a href="categories.php" class="nav-link px-2 text-white">Categories</a></li>
-                    <li class="nav-bar"><a href="accounts.php" class="nav-link px-2 text-white">Accounts</a></li>
+                    <li class="nav-bar mt-2"><a href="admin.php" class="nav-link px-2 text-white">Home</a></li>
+                    <li class="nav-bar mt-2"><a href="orders.php" class="nav-link px-2 text-white">Orders</a></li>
+                    <li class="nav-bar mt-2"><a href="reports.php" class="nav-link px-2 text-white">Reports</a></li>
+                    <li class="nav-bar mt-2"><a href="" class="nav-link px-2 text-white" id="active">Products</a></li>
+                    <li class="nav-bar mt-2"><a href="categories.php" class="nav-link px-2 text-white">Categories</a></li>
+                    <li class="nav-bar mt-2"><a href="accounts.php" class="nav-link px-2 text-white">Accounts</a></li>
                     </ul>
                     <div class="text-end">
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
